713e8cc12d606d6b512626628139bb431404f176
balakrishnan <nbalakrishna349@gmail.com>
2026-07-16 16:11:00 +0530

    VowelCount

--- phase1-core-java/syntaxDrill.java
+++ phase1-core-java/syntaxDrill.java
@@ -1,10 +1,15 @@
-import java.util.Locale;
+import java.util.*;
 
 public class syntaxDrill{
     public static void main ( String[] args) {
 
-        int result = countvowels("balakrishna");
-        System.out.println(result);
+        List<Integer> num = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        String [] words = {"Bala","Bala","Chandu"};
+
+        Map<String,Integer> output = wordFrequency(words);
+        List<Integer> result = getEvenNumbers(num);
+        System.out.println(output);
     }
 
     public static int countvowels(String sentance){
@@ -18,5 +23,37 @@ public class syntaxDrill{
             }
         }
         return count;
+    }
+
+    public static List<Integer> getEvenNumbers(List<Integer> numbers) {
+        // your code here
+        List<Integer> result = new ArrayList<>();
+        for( int i = 0; i<numbers.size();i++){
+
+            if ( numbers.get(i) % 2 == 0){
+                result.add(numbers.get(i));
+            }
+        }
+        return result;
+    }
+
+    public static Map<String, Integer> wordFrequency(String[] words) {
+
+        Map<String , Integer> result = new HashMap<>();
+
+        for ( int i =0 ; i < words.length; i++){
+
+            String word = words[i];
+
+            if ( result.containsKey(word)){
+
+                result.put(word,result.get(word)+1);
+            }
+            else{
+
+                result.put(word,1);
+            }
+        }
+        return result;
     }
 }
